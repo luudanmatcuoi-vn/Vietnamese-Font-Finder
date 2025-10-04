2a69c1d495cfbead7a72e3d3ba096bdc3aa4b028
Luudan Matcuoi <44934736+luudanmatcuoi-vn@users.noreply.github.com>
2025-10-05 04:59:54 +0700

    update font mới
    lành ít dữ nhiều 😧

--- make_font_image.py
+++ make_font_image.py
@@ -1,15 +1,19 @@
-import matplotlib.font_manager
-
+from os import listdir
+from os.path import isfile, join, isdir
 import sys, re, json, os
 sys.stdout.reconfigure(encoding='utf-8')
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import PIL.ImageOps
 import imagehash
 from scipy import ndimage
 Image.MAX_IMAGE_PIXELS = 9696447580
 
-FONT_ROOT_PATH = "D:\\rank\\vietnamese-font"
+FONT_ROOT_PATH = "F:\\Animesoftsub\\vietnamese-font"
+font_images_folder = "D:\\Vietnamese-Font-Finder\\font_images"
+
+listfile = [f for f in listdir(font_images_folder) if isfile(join(font_images_folder, f) )]
+
 fonts = []
 for path, subdirs, files in os.walk(FONT_ROOT_PATH):
     for name in files:
@@ -17,21 +21,6 @@ for path, subdirs, files in os.walk(FONT_ROOT_PATH):
             full_path = os.path.join(path,name)
             relative_path = full_path.replace(FONT_ROOT_PATH,"")[1:]
             fonts +=[ [full_path, relative_path, name ] ]
-
-forbidden_fonts = ["MTD Authemart Extras"]
-if "fonts" not in globals():
-    fonts = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-    fonts = [f for f in fonts if any([a.lower() in f.lower() for a in ["uvn","utm"]])]
-    fonts = [f for f in fonts if not any([a.lower() in f.lower() for a in forbidden_fonts])]
-    fonts = sorted(list(set(fonts)))
-    fonts = [[f, f.split("\\")[-1], f.split("\\")[-1] ] for f in fonts]
-
-# f=0
-# while f <len(fonts):
-#     fa = [a for a in fonts if a[1]==fonts[f][1] ][1:]
-#     for a in fa:
-#         fonts.remove(a)
-#     f+=1
 
 fonts = sorted(fonts)
 
@@ -46,8 +35,11 @@ def show(im):
     show_img = Image.fromarray(np.uint8(im), 'RGB')
     show_img.show()
 
-def finding_font_size(require_height, global_font, seperator, finding=True):
-    img  = Image.new('L', ( 1000, require_height*3 ))
+def finding_font_size(require_height, global_font, seperator, finding=True,save=False):
+    if save:
+        img  = Image.new('L', ( 5000, require_height*5 ))
+    else:
+        img  = Image.new('L', ( 1000, require_height*3 ))
     draw = ImageDraw.Draw(img)
 
     if finding:
@@ -73,7 +65,10 @@ def finding_font_size(require_height, global_font, seperator, finding=True):
 
     # Making np_img
     try:
-        draw.text(( 10,10 ), seperator, (255), font = font)
+        if save:
+            draw.text(( 100,100 ), seperator, (255), font = font, align ="left")
+        else:
+            draw.text(( 10,10 ), seperator, (255), font = font)
     except:
         return font_size, "0000000000000000"
     np_img = np.asarray(img)
@@ -87,6 +82,15 @@ def finding_font_size(require_height, global_font, seperator, finding=True):
             delta_width = width_list[-1]-width_list[0]
     else:
         bounding_box = ndimage.find_objects(np_img)
+        if save:
+            widthl = sorted( set(np.where(np_img >20 )[1]) )
+            heightl = sorted( set(np.where(np_img >20 )[0]) )
+            try:
+                aimg = img.crop((widthl[0]-10,heightl[0]-10,widthl[-1]+20,heightl[-1]+10))
+                aimg = ImageOps.invert(aimg)
+                aimg.save(save + ".jpg")
+            except:
+                pass
         try:
             img = img.crop((bounding_box[-1][1].start, bounding_box[-1][0].start, bounding_box[-1][1].stop, bounding_box[-1][0].stop))
             delta_width = str( imagehash.phash( img ) )
@@ -96,7 +100,7 @@ def finding_font_size(require_height, global_font, seperator, finding=True):
     return font_size, delta_width
 
 def create_map(font, characters_req):
-    font_size,_ = finding_font_size(80, font[0],"o",finding =True)
+    font_size,_ = finding_font_size(30, font[0],"o",finding =True)
     result_map = {}
     for ch in characters_req:
         a, wid = finding_font_size(font_size, font[0], ch, finding=False)
@@ -110,13 +114,41 @@ try:
 except:
     pass
 
+##Thêm các font mới vào font_hash
 for font in fonts:
     if font[1] in multi_data.keys(): continue
-    print(font)
+    print("add font", font)
     data = create_map(font, characters_req)
     multi_data[font[1]] = data
     with open('font_hash.json', 'w', encoding="utf-8") as f:
         json.dump(multi_data, f)
 
-# print(multi_data)
 
+
+##Xoá các font ko có trong host
+clannad = [font[1] for font in fonts]
+multi_data_2 = {}
+for k in multi_data.keys():
+    if k in clannad:
+        multi_data_2[k] = multi_data[k]
+    else:
+        print("remove font", multi_data[k])
+with open('font_hash.json', 'w', encoding="utf-8") as f:
+    json.dump(multi_data_2, f)
+
+
+
+##Thêm ảnh cho các font mới
+for font in fonts:
+    if font[2]+".jpg" in listfile: continue
+    print("add image", font)
+    font_size,_ = finding_font_size(80, font[0],"o",finding =True)
+    _,_ = finding_font_size(font_size, font[0], "Phông Chữ Mẫu\"?!,.", finding=False, save=font_images_folder+"\\"+font[2])
+
+
+##Xoá ảnh font mà ko có host
+clannad = [ font[2]+".jpg" for font in fonts]
+for g in listfile:
+    if g not in clannad:
+        print("remove image", g)
+        os.remove( os.path.join(font_images_folder, g) )
